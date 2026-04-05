86fe4e2526c3f4faae0c7b1ad3e776fc0fdd6f09
Nairouz Bakhta BEN HADJ SLAMA <152972131+bakhtaa@users.noreply.github.com>
2026-04-05 10:45:46 +0100

    endpoints of compaign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from random import randint
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Response
 from typing import Any
 app= FastAPI()
 
@@ -44,18 +44,44 @@ async def getcompaign(id: int):
     raise HTTPException(status_code=404)
 
 
-@app.post('/compaigns')
+@app.post('/compaigns', status_code=201)
 async def createcompaign(body: dict[str, Any]):
     
     print(body)
-    new = {
+    new : Any = {
         "campaign_id" : randint(100,1000),
         "name": body.get("name"),
-        "due_date": datetime.now(),
+        "due_date": body.get("due_date"),
         "created_at": datetime.now()
     }
-    data.append()
+    data.append(new)
     return { "compaign": new}
+
+@app.put("/compaigns/{id}")
+async def updatecompaign(id : int, body : dict[str, Any]):
+    for index, campaign in enumerate(data):
+        if campaign.get("campaign_id") == id:
+            updated_compaign :  Any= {
+                "campaign_id": campaign.get("campaign_id"),
+                "name": body.get("name"),
+                "due_date": body.get("due_date"),
+                "created_at": campaign.get("created_at")
+            }
+            data[index]=updated_compaign
+            return {"updated": updated_compaign}
+    raise HTTPException(status_code=404)
+
+@app.delete("/campaigns/{id}")
+async def deletecampaign(id: int):
+    for index,campaign in enumerate(data):
+        if campaign.get("campaign_id")==id:
+            data.pop(index)
+            return Response(status_code=204)
+
+
+
+  
+
 
 """
 campaigns:
